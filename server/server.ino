e57bae785edde4d5feaa2bcb5d2871582ff44ce0
Mariusz Nieciecki <mariusz.nieciecki@o2.pl>
2021-01-17 18:31:21 +0100

    comments part 1

--- server/server.ino
+++ server/server.ino
@@ -1,21 +1,21 @@
-#include <ObirDhcp.h>
-#include <ObirEthernet.h>
-#include <ObirEthernetUdp.h>
+#include <ObirDhcp.h>                            //bilioteka ktora umozliwia pobranie IP z DHCP - platforma dla ebsim'a
+#include <ObirEthernet.h>                        //biblioteka niezbedna dla klasy 'ObirEthernetUDP'
+#include <ObirEthernetUdp.h>                     //biblioteka z klasa 'ObirEthernetUDP'
 
-#include "coap.h"
+#include "coap.h"                                //biblioteka coap zawierajaca rozne typy zmiennych
 
-#define UDP_SERVER_PORT 1234
-#define PACKET_BUFFER_LEN 50
+#define UDP_SERVER_PORT 1234                     //port z ktoego korzystamy w tym projekcie
+#define PACKET_BUFFER_LEN 50                     //dlugosc pakietu z danymi dla/z UDP
 
 byte MAC[]={0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x01}; //MAC adres karty sieciowej, to powinno byc unikatowe - proforma dla ebsim'a 
 
-uint8_t packetBuffer [PACKET_BUFFER_LEN];
+uint8_t packetBuffer [PACKET_BUFFER_LEN];        //tablica o dlugosci pakietu
 
-ObirEthernetUDP Udp;
+ObirEthernetUDP Udp;                             //obiekt klasy 'ObirEthernetUDP'
 
-int localport = UDP_SERVER_PORT;
+int localport = UDP_SERVER_PORT;                 //numer portu na jakim bedziemy nasluchiwac
 
-void PrintHeader(CoapHeader& header)
+void PrintHeader(CoapHeader& header)             //funkcja wypisujaca parametry obslugiwanych wiadomosci
 {
    Serial.println(F("----HEADER----"));
    Serial.print(F("Ver: "));Serial.println(header.ver, DEC);
@@ -27,43 +27,43 @@ void PrintHeader(CoapHeader& header)
 }
 
 void setup() {
+  //Przywitanie z uzytkownikiem - potwierdzajace poprawne uruchomienie sie systemu
   Serial.begin(115200);
   Serial.print(F("OBIR PROJEKT INIT..."));Serial.print(F(__FILE__));
   Serial.print(F(", "));Serial.print(F(__DATE__));Serial.print(F(", "));Serial.print(F(__TIME__));Serial.println(F("]")); 
 
-  ObirEthernet.begin(MAC);
+  ObirEthernet.begin(MAC);                          //inicjacja karty sieciowej - preforma dla ebsim'a 
 
-  Serial.print(F("My IP address: "));
+  Serial.print(F("My IP address: "));               //wyswietlenie informacji potwierdzajacej na jakim IP dzialamy
     for (byte thisByte = 0; thisByte < 4; thisByte++) {
         Serial.print(ObirEthernet.localIP()[thisByte], DEC);Serial.print(F("."));
     }
     Serial.println();
-
-    //Uruchomienie nasluchiwania na datagaramy UDP
-    Udp.begin(localport);
+    
+    Udp.begin(localport);                            //Uruchomienie nasluchiwania na datagaramy UDP
 }
 
 void loop() {
-  int packetSize = Udp.parsePacket();
-  
-  if(packetSize>0){
-    int len = Udp.read(packetBuffer, PACKET_BUFFER_LEN);
+  int packetSize = Udp.parsePacket();                
+  if(packetSize>0){                                       //instrkcja warunkowa sprawdzajaca jaka jest dligosc pakietu, 
+                                                          //jesli jest <=0 to nic nie otrzymalismy
+    int len = Udp.read(packetBuffer, PACKET_BUFFER_LEN);  //odczytujemy pakiet(maksymalnie do liczby bajtów = PACKET_BUFER_LEN)
 
     Serial.println(F("----------------MESSAGE--------------"));
     //Serial.println((char*)packetBuffer);
 
     uint8_t ver = (0xC0 & packetBuffer[0])>>6;
-    uint8_t type = (0x30 & packetBuffer[0])>>4; // 1=NON, 0 =CON
+    uint8_t type = (0x30 & packetBuffer[0])>>4;                    //1=NON, 0 =CON
     uint8_t _token_len = (0xF & packetBuffer[0]);
     uint8_t _class = ((packetBuffer[1]>>5)&0x07);
     uint8_t _code = ((packetBuffer[1]>>0)&0x1F);
-    uint16_t _mid = (packetBuffer[2]<<8)|(packetBuffer[3]); //Message ID - ma 2 bajty
+    uint16_t _mid = (packetBuffer[2]<<8)|(packetBuffer[3]);        //Message ID - ma 2 bajty
     
-    uint8_t _token[_token_len]; //latwiej niz tworzyc 8 opcji roznych dlugosci int
+    uint8_t _token[_token_len];                                    //latwiej niz tworzyc 8 opcji roznych dlugosci int
     
-    CoapHeader header(ver, type, _token_len, _class, _code, _mid);
+    CoapHeader header(ver, type, _token_len, _class, _code, _mid); //obiekt klasy CoapHeader
     
-    PrintHeader(header);
+    PrintHeader(header);                                           //wywolanie funkcji wypisujacej parametry wiadomosci
 
    if(_token_len > 0) 
     {
